c2ea56ef1965e1050d0042803c8a2de5b5dcf0c7
Camillo <eduardo.camillo15@gmail.com>
2024-05-13 21:50:56 -0300

    First commit

---
+++ crudProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace crudProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ crudProject/Models/UsuarioModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+
+
+namespace crudProject.Models
+{
+   
+    public class UsuarioModel : IDisposable
+    {
+        private SqlConnection connection;
+
+        public UsuarioModel()
+        {
+            string conn = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BDContato;Integrated Security=True";
+        
+            connection = new SqlConnection(conn);
+            connection.Open();
+        
+        }
+        public void Dispose()
+        {
+            connection.Close();
+        }
+
+        public void Create(UsuarioWeb usuario)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"INSERT INTO Usuarios VALUES (@nome, @email)";
+
+            cmd.Parameters.AddWithValue("@nome", usuario.nome);
+            cmd.Parameters.AddWithValue("@email", usuario.email);
+
+            cmd.ExecuteNonQuery();
+        }
+        public List<UsuarioWeb> Read()
+        {
+            List<UsuarioWeb> lista = new List<UsuarioWeb>();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+            cmd.CommandText = @"SELECT * FROM Usuarios";
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            
+            while(reader.Read())
+            {
+                UsuarioWeb usuario = new UsuarioWeb();
+                usuario.id = (int)reader["ID"];
+                usuario.nome = (string)reader["nome"];
+                usuario.email = (string)reader["email"];
+
+                lista.Add(usuario);
+            }
+            return lista;
+        }
+    }
+
+   
+}
